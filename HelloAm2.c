d564e62029269de2562010f9bbdf8b4aa07e7710
RiverGod <benexx1grvc@gmail.com>
2021-05-24 00:35:50 +0300

    five_commit_by_master_???

--- HelloAm2.c
+++ HelloAm2.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 
-void hello()
+void hello(void)
 {
 	printf("Hello Amazon!");
+	return;
+}
+
+void karman(void)
+{
+	printf("Derji karman shire");
+	return;
+}
+void grok (void)
+{
+	printf("Ya tebya groknu");
 	return;
 }
 
 void main (void)
 {
 	hello();
+	karman();
+	grok();
 	return;
 }
